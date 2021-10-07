9e11c9fab6610f4c5fe930aa97ff860fd64b0c48
Carlos Hinostroza <bryan_hinostroza.com>
2021-10-07 17:53:06 -0500

    Mi primer commit

---
+++ PruebaNet5Core/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PruebaNet5Core.Controllers
+{
+    public class HomeController : Controller
+    {
+    
+        public ViewResult Index()
+        {
+            ViewBag.Dato = "Saludos desde la vista";
+            return View();
+        }
+
+    }
+}
