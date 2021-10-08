a1b145df3ff89ad0fcfd0602e48e2af87a40c472
Carlos Hinostroza <bryan_hinostroza.com>
2021-10-08 10:32:30 -0500

    Nueva rama proveedor

--- PruebaNet5Core/Models/Amigo.cs
+++ PruebaNet5Core/Models/Amigo.cs
@@ -7,5 +7,24 @@ namespace PruebaNet5Core.Models
 {
     public class Amigo
     {
+
+        private int id;
+        private string nombres;
+        private string correo;
+
+        public int Id { get => id; set => id = value; }
+        public string Nombres { get => nombres; set => nombres = value; }
+        public string Correo { get => correo; set => correo = value; }
+
+        public Amigo()
+        {
+        }
+
+        public Amigo(int id, string nombres, string correo)
+        {
+            this.id = id;
+            this.nombres = nombres;
+            this.correo = correo;
+        }
     }
 }
